32cf6dd7cc539f3cf288fbb64fb1c9ab94d55329
Bogdan-K-A <kozhoma_b@ukr.net>
2021-11-09 17:44:40 +0200

    1

--- src/js/clearLibrary.js
+++ src/js/clearLibrary.js
@@ -1,17 +1,16 @@
 import clearLibrary from '../templates/clear-library.hbs';
 const galleryLib = document.querySelector('.content');
-  const backHome = document.querySelector('.clear-library_btn');
+const backHome = document.querySelector('.clear-library_btn');
 
 function refreshPage() {
   document.location.reload();
 }
 
 function clearLib() {
+  galleryLib.innerHTML = '';
+  galleryLib.insertAdjacentHTML('beforeend', clearLibrary());
 
-galleryLib.innerHTML = '';
-galleryLib.insertAdjacentHTML('beforeend', clearLibrary());
-
-backHome.addEventListener('click', refreshPage);
+  backHome.addEventListener('click', refreshPage);
 }
 
-clearLib()
+// clearLib()
