a15b304476633a4de02f029ae0fd5ea492bc38ad
Danylo Nehreba <butanasiland@gmail.com>
2021-11-14 19:13:55 +0200

    fixset bag

--- src/js/addToLocalStorLibrary.js
+++ src/js/addToLocalStorLibrary.js
@@ -1,3 +1,5 @@
+import { getLibrary } from './linkMyLibrary.js';
+
 const modalDiv = document.querySelector('.modal_backdrop');
 
 renderWatch();
@@ -31,33 +33,45 @@ export function addToLocalStorWatched() {
     // добавляє фільм
     if (btnWatch.textContent === 'add to Watched') {
       btnWatch.textContent = 'remowe to Watched';
-      btnWatch.classList.add('active')
-      // забирає обєкт з фільмом який зараз на модальному вікні і розпарсує його
-      const films = localStorage.getItem('currentFilm');
-      const addFilms = JSON.parse(films);
+      btnWatch.classList.add('active');
 
-      // забирає обєкт з LocalStoreg і розпарсує його
-      const currentStore = localStorage.getItem('watched');
-      const parseStore = JSON.parse(currentStore);
-      // добавляє фільм в обєкт і записує в LocalStoreg
-      parseStore.push(addFilms);
-      localStorage.setItem('watched', JSON.stringify(parseStore));
+      addLocalStorObjectWatch();
 
       // видаляє фільм
     } else if (btnWatch.textContent === 'remowe to Watched') {
       btnWatch.textContent = 'add to Watched';
-      btnWatch.classList.remove('active')
+      btnWatch.classList.remove('active');
 
-      // забирає обєкт з LocalStoreg і розпарсує його
-      const currentStore = localStorage.getItem('watched');
-      const parseStore = JSON.parse(currentStore);
-      // забирає обєкт з фільмом який зараз на модальному вікні і розпарсує його
-      const films = localStorage.getItem('currentFilm');
-      const addFilms = JSON.parse(films);
+      remoweLocalStorObjectWatch();
 
-      // находить і видаляє фільм з обєкта і перезаписує в LocalStoreg
-      const removeFilms = parseStore.filter(e => e.id !== addFilms.id);
-      localStorage.setItem('watched', JSON.stringify(removeFilms));
+      // обновляння сторінки при видаляня фільму
+      const watchedEl = document.querySelector('.js-btn-watched');
+      const libraryEl = document.querySelector('.site-nav__link-library');
+      const wrapperFilms = document.querySelector('.wrapper-films');
+
+      if (libraryEl.classList.contains('link__current')) {
+        wrapperFilms.innerHTML = '';
+
+        remoweLocalStorObjectWatch();
+
+        getLibrary();
+      } else {
+        btnWatch.textContent = 'add to Watched';
+        btnWatch.classList.remove('active');
+
+        remoweLocalStorObjectWatch();
+      }
+
+      if (watchedEl.classList.contains('isActive')) {
+        wrapperFilms.innerHTML = '';
+        remoweLocalStorObjectWatch();
+        getLibrary();
+      } else {
+        btnWatch.textContent = 'add to Watched';
+        btnWatch.classList.remove('active');
+
+        remoweLocalStorObjectWatch();
+      }
     }
   }
 }
@@ -73,36 +87,47 @@ export function addToLocalStorQueue() {
 
     // добавляє фільм
     if (btnQueue.textContent === 'add to queue') {
-        btnQueue.textContent = 'remowe to queue';
-        btnQueue.classList.add('active')
-      // забирає обєкт з фільмом який зараз на модальному вікні і розпарсує його
-      const films = localStorage.getItem('currentFilm');
-      const addFilms = JSON.parse(films);
+      btnQueue.textContent = 'remowe to queue';
+      btnQueue.classList.add('active');
 
-      // забирає обєкт з LocalStoreg і розпарсує його
-      const currentStore = localStorage.getItem('queue');
-      const parseStore = JSON.parse(currentStore);
-
-      // добавляє фільм в обєкт і записує в LocalStoreg
-      parseStore.push(addFilms);
-      localStorage.setItem('queue', JSON.stringify(parseStore));
+      addLocalStorObjectQueue();
 
       // видаляє фільм
     } else if (btnQueue.textContent === 'remowe to queue') {
+      btnQueue.textContent = 'add to queue';
+      btnQueue.classList.remove('active');
+
+      remoweLocalStorObjectQueue();
+
+      const queueEl = document.querySelector('.js-btn-queue');
+      const libraryEl = document.querySelector('.site-nav__link-library');
+      const wrapperFilms = document.querySelector('.wrapper-films');
+
+      if (libraryEl.classList.contains('link__current')) {
+        wrapperFilms.innerHTML = '';
+
+        remoweLocalStorObjectQueue();
+
+        getLibrary();
+      } else {
         btnQueue.textContent = 'add to queue';
-        btnQueue.classList.remove('active')
+        btnQueue.classList.remove('active');
 
-      // забирає обєкт з LocalStoreg і розпарсує його
-      const currentStore = localStorage.getItem('queue');
-      const parseStore = JSON.parse(currentStore);
+        remoweLocalStorObjectQueue();
+      }
 
-      // забирає обєкт з фільмом який зараз на модальному вікні і розпарсує його
-      const films = localStorage.getItem('currentFilm');
-      const addFilms = JSON.parse(films);
+      if (queueEl.classList.contains('isActive')) {
+        wrapperFilms.innerHTML = '';
 
-      // находить і видаляє фільм з обєкта і перезаписує в LocalStoreg
-      const removeFilms = parseStore.filter(e => e.id !== addFilms.id);
-      localStorage.setItem('queue', JSON.stringify(removeFilms));
+        remoweLocalStorObjectQueue();
+
+        getLibrary();
+      } else {
+        btnQueue.textContent = 'add to queue';
+        btnQueue.classList.remove('active');
+
+        remoweLocalStorObjectQueue();
+      }
     }
   }
 }
@@ -120,8 +145,8 @@ export function renderBtnWatch() {
 
   // перевірка кнопки
   if (parseStore.some(e => e.id === addFilms.id)) {
-      btnWatch.textContent = 'remowe to Watched';
-      btnWatch.classList.add('active')
+    btnWatch.textContent = 'remowe to Watched';
+    btnWatch.classList.add('active');
   }
 }
 
@@ -139,7 +164,62 @@ export function renderBtnQueue() {
 
   // перевірка кнопки
   if (parseStore.some(e => e.id === addFilms.id)) {
-      btnQueue.textContent = 'remowe to queue';
-      btnQueue.classList.add('active')
+    btnQueue.textContent = 'remowe to queue';
+    btnQueue.classList.add('active');
   }
+}
+
+// додає фільм на localStor в обєкт Wached
+function addLocalStorObjectWatch() {
+  // забирає обєкт з фільмом який зараз на модальному вікні і розпарсує його
+  const films = localStorage.getItem('currentFilm');
+  const addFilms = JSON.parse(films);
+
+  // забирає обєкт з LocalStoreg і розпарсує його
+  const currentStore = localStorage.getItem('watched');
+  const parseStore = JSON.parse(currentStore);
+  // добавляє фільм в обєкт і записує в LocalStoreg
+  parseStore.push(addFilms);
+  localStorage.setItem('watched', JSON.stringify(parseStore));
+}
+// видаляє фільм з обекта Watched на Local Soreg
+function remoweLocalStorObjectWatch() {
+  // забирає обєкт з LocalStoreg і розпарсує його
+  const currentStore = localStorage.getItem('watched');
+  const parseStore = JSON.parse(currentStore);
+  // забирає обєкт з фільмом який зараз на модальному вікні і розпарсує його
+  const films = localStorage.getItem('currentFilm');
+  const addFilms = JSON.parse(films);
+
+  // находить і видаляє фільм з обєкта і перезаписує в LocalStoreg
+  const removeFilms = parseStore.filter(e => e.id !== addFilms.id);
+  localStorage.setItem('watched', JSON.stringify(removeFilms));
+}
+// додає фільм на localStor в обєкт Queue
+function addLocalStorObjectQueue() {
+  // забирає обєкт з фільмом який зараз на модальному вікні і розпарсує його
+  const films = localStorage.getItem('currentFilm');
+  const addFilms = JSON.parse(films);
+
+  // забирає обєкт з LocalStoreg і розпарсує його
+  const currentStore = localStorage.getItem('queue');
+  const parseStore = JSON.parse(currentStore);
+
+  // добавляє фільм в обєкт і записує в LocalStoreg
+  parseStore.push(addFilms);
+  localStorage.setItem('queue', JSON.stringify(parseStore));
+}
+// видаляє фільм з обекта Queue на Local Soreg
+function remoweLocalStorObjectQueue() {
+  // забирає обєкт з LocalStoreg і розпарсує його
+  const currentStore = localStorage.getItem('queue');
+  const parseStore = JSON.parse(currentStore);
+
+  // забирає обєкт з фільмом який зараз на модальному вікні і розпарсує його
+  const films = localStorage.getItem('currentFilm');
+  const addFilms = JSON.parse(films);
+
+  // находить і видаляє фільм з обєкта і перезаписує в LocalStoreg
+  const removeFilms = parseStore.filter(e => e.id !== addFilms.id);
+  localStorage.setItem('queue', JSON.stringify(removeFilms));
 }
