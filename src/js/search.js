5f8e8f429538990092baeb0efbc6c89d05f21cb2
Natalia Odinets <natashaodinec57@gmail.com>
2021-11-15 22:19:41 +0200

    Добавляет пагинацию библиотеки  и поисковика

--- src/js/search.js
+++ src/js/search.js
@@ -4,10 +4,13 @@ const fetchDataByQuery = new API();
 import mainGallery from '../templates/card-film.hbs';
 
 import genresData from './data/genresData.json';
+import { getCard, updateDate, updateGenres, updateRating } from './getCards.js';
 import { onCutDate, onToggleGenresData } from './components/newData';
-import { updateGenres } from './getCards'
+// import { updateGenres } from './getCards'
+import { switchesPages } from './pagination'
 const { galleryList, inputQuery, inputForm, errorMsg } = refs;
 
+const pagination = document.querySelector('.js-pagination');
 inputForm.addEventListener('submit', onSearchSubmit);
 
 async function onSearchSubmit(e) {
@@ -18,14 +21,21 @@ async function onSearchSubmit(e) {
     return;
   }
   try {
+    pagination.removeEventListener('click', switchesPages);
+    pagination.addEventListener('click', switchesInputPages);
+      // pagination.innerHTML = '';
+  let page = 1;
+    createPageInput(page)
     const data = await fetchDataByQuery.getQueryMovie(inputQuery.value);
+    
     fetchDataByQuery.query = inputQuery.value;
     if (typeof data.results === 'undefined' || data.results.length < 1) {
+     
       errorMsg.innerHTML =
         'Search result not successful. Enter the correct movie name and try again';
       return;
     }
-    onCutDate(data);
+    // onCutDate(data);
     updateGenres(data)
     // onToggleGenresData(data, genresData);
     const markup = mainGallery(data);
@@ -33,4 +43,138 @@ async function onSearchSubmit(e) {
   } catch (err) {
     console.log('fetchDataByQuery error');
   }
+}
+
+async  function createPageInput(page) {
+ 
+ let liTag = '';
+  let activeLi;
+  let beforePage = page - 1; // 20 - 1 = 19
+  let afterPage = page + 1; // 20 + 1 = 21
+ 
+  const data = await fetchDataByQuery.getQueryMovie(inputQuery.value);
+  let totalPage = data.total_pages;
+  console.log(totalPage);
+/* ------------------------- добавляет стрелку влево ------------------------ */
+  if (page > 1) {
+    //если значение страницы больше 1, добавляем новый li, который является предыдущей кнопкой
+    // liTag += `<li class="pagination-arrow" data-index="${page - 1}"><svg class="icon">
+    //                 <use href="./images/icon/icons.svg#icon-arrow-left"></use>
+    //             </svg></li>`;
+
+    // ----------------------------------------
+    liTag += `<li class="pagination-arrow" data-index="${
+      page - 1
+    }"><svg class="pag-icon" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+        d="M12.6666 8H3.33325"
+
+      />
+    <path
+        d="M7.99992 12.6667L3.33325 8.00004L7.99992 3.33337"
+
+      />
+    </svg></li>`;
+    // ----------------------------------------
+  }
+
+  /* ---------------------- добавляет ... вначале после 1 --------------------- */
+  if (page > 2) {
+    //если значение страницы больше 2, добавляем новый тег li с значением 1
+    liTag += `<li class="num" data-index="1">1</li>`;
+
+    if (page > 3) {
+      //если значение страницы больше 3, добавляем новый тег li с значением ...
+      liTag += `<li class="dots"><span>. . .</span></li>`;
+
+      if (page > 4) {
+        beforePage -= 1;
+      }
+      afterPage += 1;
+    }
+  }
+
+  /* ----- сколько страниц или li показывают до текущего li с левого краю ----- */
+  if (page === totalPage) {
+    //если значение страницы равно общему количеству страниц, вычти -2 из значения предыдущей страницы
+    afterPage+=2;
+  } else if (page === totalPage - 1) {
+    //а если значение страницы равно общему количеству страниц -1, вычти -1 из значения предыдущей страницы
+    afterPage+=1;
+  }
+
+  /* ----------- сколько страниц или li показывают после текущего li с правого краю ---------- */
+  if (page === 1) {
+    //если значение страницы равно 1, добавь +2 к значению после страницы
+   beforePage;
+  }
+  else if (page === 2) {
+    //а если значение страницы равно 2, добавь +1 к значению после страницы
+    beforePage -= 1;
+  }
+
+  /* --------------------------- добалляет нумерацию -------------------------- */
+for (let pageLength = beforePage; pageLength <= afterPage; pageLength++) {
+    if (pageLength > totalPage) {
+      continue;
+    }
+    if (pageLength === 0) {
+      //если pageLangs равно 0, добавляем +1 к значению pageLangth
+      pageLength += 1;
+    }
+
+    /* ----------------------- указывает активную страницу ---------------------- */
+    if (page === pageLength) {
+      //если значение страницы равно pageLength, тогда назначаем активную строку из переменной activeLy
+      activeLi = 'active';
+    } else {
+      // в противном случае оставляем пустую строку в переменной activeLi
+      activeLi = '';
+    }
+
+    liTag += `<li class="num ${activeLi}" data-index="${pageLength}">${pageLength}</li>`;
+  }
+
+  /* --------------- добавляет ... вконце перед последней цифрой -------------- */
+  if (page < totalPage - 2) {
+    //если значение страницы меньше totalPage на -1, то показать последний li или страницу, которая равна 20
+
+    if (page < totalPage - 2) {
+      //если значение страницы меньше totalPage на -2, тогда показывать последний ... предпоследний
+      liTag += `<li class="dots"><span>. . .</span></li>`;
+    }
+
+    liTag += `<li class="num"  data-index="${totalPage}">${totalPage}</li>`;
+  }
+
+  /* ------------------------ добавляет стрелку вправо ------------------------ */
+  if (page < totalPage) {
+    //если значение страницы меньше общего значения страницы тогда, добавьте новый li, который является следующей кнопкой
+    // liTag += `<li class="pagination-arrow"  data-index="${page + 1}" ><svg class="icon">
+    //                 <use href="./images/icon/arrow-r.svg"></use>
+    //             </svg></li>`;
+
+  //   // ---------------------------------
+    liTag += `<li class="pagination-arrow"  data-index="${
+      page + 1
+    }"><svg class="pag-icon" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M3.33341 8H12.6667"/>
+    <path d="M8.00008 12.6667L12.6667 8.00004L8.00008 3.33337" />
+    </svg></li>`;
+    // ---------------------------------
+  }
+  pagination.innerHTML = liTag;
+}
+async function switchesInputPages(e) {
+  if (e.target.tagName !== 'LI') return;
+
+  galleryList.innerHTML = '';
+
+  fetchDataByQuery.page = +e.target.dataset.index;
+
+  const data = await fetchDataByQuery.getQueryMovie(inputQuery.value);
+
+  createPageInput(+e.target.dataset.index);
+
+  galleryList.insertAdjacentHTML('beforeend', mainGallery(data));
 }
