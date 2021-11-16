d3c9498fedc2aca35dc277928fc21b73d3faa984
Bogdan-K-A <kozhoma_b@ukr.net>
2021-11-16 14:23:13 +0200

    fix

--- src/js/getCards.js
+++ src/js/getCards.js
@@ -7,10 +7,9 @@ const service = new ApiService();
 
 export async function getCard() {
   try {
-
     spinerAdd();
     const data = await service.fetchFilms();
-   
+
     const genre = await service.fetchGenre();
 
     updateDate(data);
@@ -29,9 +28,9 @@ export async function getCard() {
 export function updateDate(data) {
   data.results.map(el => {
     if (el.release_date === undefined) {
-      return
+      return;
     }
-    console.log(el.release_date);
+    // console.log(el.release_date);
     el.release_date = el.release_date.substring(0, 4);
   });
 
@@ -86,4 +85,3 @@ export function createCardFilm(data) {
 }
 
 getCard();
-
